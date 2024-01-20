aa467da30f969c0d07631074cad6685069488fbf
Wobins <azalyange19@gmail.com>
2024-01-21 00:21:33 +0100

    Added footer component and a tab for the graph on the home page

---
+++ src/_components/footer/index.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Link from 'next/link'
+import {Typography} from '@mui/material';
+
+const Footer = () => {
+    return (
+        <footer className='footer mt-3'>
+            <Typography variant="body2" color="text.secondary" align="center" className='m-0 py-3'>
+                {'Copyright © '}
+                <Link color="inherit" href="https://github.com/Wobins" target="_blank">
+                    Ange Wobinwo
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,61 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { 
+  Stack,
+  Button,
+  Tabs,
+  Tab,
+  Box,
+  Typography
+} from '@mui/material'
 import NavBar from '@/_components/navbar'
 import SearchBar from '@/_components/searchbar'
 
+
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const Home = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     // <main className={styles.main}>
     //   <div className={styles.description}>
@@ -95,18 +147,39 @@ const Home = () => {
     // </main>
 
     <main>
-      <section className="hero-section container mx-auto">
-        <div className="hero-title text-center">
+      <section id="main-section" className="border border-4 hero-section container mx-auto grid grid-cols-12 gap-2">
+        <div className="border border-3 hero-title text-center col-start-3 col-span-8 py-12">
           <h1>Check your bill</h1>
-          <p>
+          <p className="my-8">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit quae officia quas saepe sequi cupiditate animi ipsum molestias, nemo sit temporibus officiis dolore voluptatibus mollitia ut totam rem, quod aliquam.
           </p>
-        </div>
-        <div>
-          <SearchBar />
+          <Stack direction="row" spacing={3}>
+            <SearchBar />
+            <span>or</span>
+            <Button variant="contained" color="primary">Read</Button>
+          </Stack>
         </div>
       </section>
-      <section className="details-section"></section>
+      <section id="graph-section" className="details-section">
+        <div className='mx-5 border border-5'>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Item One" {...a11yProps(0)} />
+              <Tab label="Item Two" {...a11yProps(1)} />
+              <Tab label="Item Three" {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            Item One
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            Item Three
+          </CustomTabPanel>
+        </div>
+      </section>
     </main>
 
   );
